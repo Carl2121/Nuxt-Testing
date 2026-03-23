4d867d4410d8cdfafbcbe4ac2e7387c1ac4457fa
Carlos Heredero <herederocarlos10@gmail.com>
2026-03-23 12:30:32 +0800

    installed tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image']
-})
+  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
